9290ae6bba73ced450974ad7563f2df31a5aab3b
Paul <paul.shibanov@gmail.com>
2016-04-14 16:38:49 +0400

    Make angular-route work Closes #25, Closes #27

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,4 +1,16 @@
-angular.module('hello', [])
-  .controller('home', function($scope) {
-    $scope.greeting = {id: 'xxx', content: 'Hello World!'}
+var storeApp = angular.module('store', [
+  'ngRoute'
+])
+.controller('StoreCtrl', function($scope) {
+	$scope.greeting = {id: 'xxx', content: 'Hello World!'}
 })
+.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/cart', {
+        templateUrl: 'partials/cart.html',
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }])
