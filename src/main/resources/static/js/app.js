38bb558dfebfcb034a8e9321674a9049eedf7dac
Paul <paul.shibanov@gmail.com>
2016-04-19 13:13:45 +0400

    Removed leftover

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -10,7 +10,6 @@ angular.module('app', [])
     templateUrl: 'partials/pizza.html',
     link: function(scope) {
         scope.checked = scope.data[1] || scope.data[0];
-        console.log('Hello pizza', scope.data);
     }
   }
 });
