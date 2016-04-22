dd342fa1d5e05f8443f3536a88d544f2e7c08a49
Paul <paul.shibanov@gmail.com>
2016-04-22 11:38:54 +0400

    Removed leftover

--- src/main/java/com/github/unkindest/controllers/CartController.java
+++ src/main/java/com/github/unkindest/controllers/CartController.java
@@ -4,7 +4,6 @@ import org.springframework.session.web.http.HttpSessionManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
