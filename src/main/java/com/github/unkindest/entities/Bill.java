00e3b5b17cb9634803c9074f1204b1c1451377f0
Paul <paul.shibanov@gmail.com>
2016-04-28 19:57:52 +0400

    Remoded unused imports.

--- src/main/java/com/github/unkindest/entities/Bill.java
+++ src/main/java/com/github/unkindest/entities/Bill.java
@@ -1,8 +1,6 @@
 package com.github.unkindest.entities;
 
 import javax.persistence.*;
-import java.sql.Time;
-import java.sql.Timestamp;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
